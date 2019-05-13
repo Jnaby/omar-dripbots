fae62e27c40fb613cb3677c56cd70e11dacb16cd
Jnaby <36682528+Jnaby@users.noreply.github.com>
2019-05-13 21:16:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,8 +32,7 @@ client.on('ready', () => {
 
 });
  
-const Discord = require("discord.js");
-const client = new Discord.Client();
+
 
 client.on('message', message => {
 if (message.content === "test") {
